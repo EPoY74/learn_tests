2c805a09c907b8a43d659981c38c7edc642996a1
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-05 14:21:04 +0500

    Добавил несколько кейсов для деления на отрицательные числа

--- tests/test_division_func.py
+++ tests/test_division_func.py
@@ -1,4 +1,5 @@
 """Тестирую деление
+https://youtu.be/1HtEPEn4-LY?si=9JqaJE9hXowgrSg1 
 """
 import pytest
 
@@ -9,3 +10,20 @@ def test_is_division_good():
     """Тестирую деление
     """
     assert division(10,2) == 5
+
+
+def test_is_division_good_negdtiv_1():
+    """Делим А на -В
+    """
+    assert division(10,-2) == -5
+
+
+def test_is_division_good_negdtiv_2():
+    """Делим -А на В
+    """
+    assert division(-12,-2) == -6
+
+def test_is_division_good_negdtiv_3():
+    """Делим -А на -В
+    """
+    assert division(-10,-2) == 5
